75d425835e2c20b39fa97cb7ab81673a9277353a
Fedor Markovich <Fedor@MacBook-Air-Fedor.local>
2019-03-24 12:08:27 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function solveSudoku(matrix) {
-  
+module.exports = function solveSudoku(matrix)
+{
+  var matrixDone = matrix;
+
+
+  function solved(matrix)
+{
+  var cell = [];
+  if(!findEmpSpc(matrix, cell))
+  {
+    return true;
+  }
+  var row = cell[0];
+  var col = cell[1];
+  for(var num = 1;num<10;num++)
+  {
+    if(!checkRow(matrix, row, num) && !checkCol(matrix, col, num) && !checkBox(matrix,row - row % 3, col - col % 3,num))
+    {
+      matrix[row][col] = num;
+      if(solved(matrix))
+      {
+        return true;
+      }
+      matrix[row][col] = 0;
+    }
+  }
+  return false;
+}
+
+
+
+ 
+  return false;
 }
