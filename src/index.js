261e562ffa4dc53163751e6fef00b8a175ba060f
Fedor Markovich <Fedor@MacBook-Air-Fedor.local>
2019-03-24 13:13:33 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,65 +1,53 @@
-module.exports = function solveSudoku(matrix)
-{
-  var matrixDone = matrix;
+module.exports = function solveSudoku(matrix){
   
-  function check(matrix, row, col, num){
-    for(var i = 0; i < 3; i++){
-      for(var j = 0; j < 3; j++){
-        if(matrix[i + row][j + col] == num){
+  var matrixDone = matrix;
+  if(solved(matrix)){
+    matrixDone = matrix;
+  }
+  return matrixDone;
+
+  function solved(matrix){
+    var cell = [];
+    if(!fij(matrix, cell)){
+      return true;
+    }
+
+var row = cell[0];
+var col = cell[1];
+
+for(var num = 1; num < 10; num++){
+  if(!rowChecked(matrix, row, num) && !colChecked(matrix, col, num) && !check(matrix, row - row % 3, col - col % 3, num)){
+    matrix[row][col] = num;
+      if(solved(matrix)){
+        return true;
+      }
+     matrix[row][col] = 0;
+  }
+}
+return false;
+}
+
+function fij(matrix, cell){
+  for(var i = 0; i < 9; i++){
+    for(var j = 0; j < 9; j++){
+      if(matrix[i][j] == 0){
+        cell[0] = i;
+        cell[1] = j;
         return true;
       }
     }
   }
-
-  function solved(matrix){
-  var cell = [];
-  if(!fij(matrix, cell)){
-    return true;
-  }
-
-  var row = cell[0];
-  var col = cell[1];
-
-  for(var num = 1; num < 10; num++){
-    if(!rowChecked(matrix, row, num) && !colChecked(matrix, col, num) && !check(matrix, row - row % 3, col - col % 3, num)){
-      matrix[row][col] = num;
-        if(solved(matrix)){
-          return true;
-        }
-       matrix[row][col] = 0;
-    }
-  }
   return false;
 }
-
-if(solved(matrix)){
-  matrixDone = matrix;
-}
-
-return matrixDone;
-}
-  function fij(matrix, cell){
-    for(var i = 0; i < 9; i++){
-      for(var j = 0; j < 9; j++){
-        if(matrix[i][j] == 0){
-          cell[0] = i;
-          cell[1] = j;
-          return true;
-        }
+  
+function rowChecked(matrix, row, num){
+  for(var i = 0; i < 9; i++){
+    if(matrix[row][i] == num){
+      return true;
       }
     }
-    return false;
-  }
-  
-  function rowChecked(matrix, row, num){
-    for(var i = 0; i < 9; i++){
-      if(matrix[row][i] == num)
-        {
-          return true;
-        }
-      }
-    return false;
-  }
+  return false;
+}
   
   function colChecked(matrix, col, num){
     for(var i = 0; i < 9; i++){
@@ -69,6 +57,16 @@ return matrixDone;
     }
     return false;
   }
+
+  function check(matrix, row, col, num){
+    for(var i = 0; i < 3; i++){
+      for(var j = 0; j < 3; j++){
+        if(matrix[i + row][j + col] == num){
+        return true;
+      }
+    }
+}
   
 return false;
+}
 }
