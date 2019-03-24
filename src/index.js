3cc3e7380b38a2f20fc9812b8240bd586a9a3bee
Fedor Markovich <Fedor@MacBook-Air-Fedor.local>
2019-03-24 13:07:37 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,34 +1,74 @@
 module.exports = function solveSudoku(matrix)
 {
   var matrixDone = matrix;
-
-
-  function solved(matrix)
-{
-  var cell = [];
-  if(!findEmpSpc(matrix, cell))
-  {
-    return true;
-  }
-  var row = cell[0];
-  var col = cell[1];
-  for(var num = 1;num<10;num++)
-  {
-    if(!checkRow(matrix, row, num) && !checkCol(matrix, col, num) && !checkBox(matrix,row - row % 3, col - col % 3,num))
-    {
-      matrix[row][col] = num;
-      if(solved(matrix))
-      {
+  
+  function check(matrix, row, col, num){
+    for(var i = 0; i < 3; i++){
+      for(var j = 0; j < 3; j++){
+        if(matrix[i + row][j + col] == num){
         return true;
       }
-      matrix[row][col] = 0;
+    }
+  }
+
+  function solved(matrix){
+  var cell = [];
+  if(!fij(matrix, cell)){
+    return true;
+  }
+
+  var row = cell[0];
+  var col = cell[1];
+
+  for(var num = 1; num < 10; num++){
+    if(!rowChecked(matrix, row, num) && !colChecked(matrix, col, num) && !check(matrix, row - row % 3, col - col % 3, num)){
+      matrix[row][col] = num;
+        if(solved(matrix)){
+          return true;
+        }
+       matrix[row][col] = 0;
     }
   }
   return false;
 }
 
+if(solved(matrix)){
+  matrixDone = matrix;
+}
 
-
- 
-  return false;
+return matrixDone;
+}
+  function fij(matrix, cell){
+    for(var i = 0; i < 9; i++){
+      for(var j = 0; j < 9; j++){
+        if(matrix[i][j] == 0){
+          cell[0] = i;
+          cell[1] = j;
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  function rowChecked(matrix, row, num){
+    for(var i = 0; i < 9; i++){
+      if(matrix[row][i] == num)
+        {
+          return true;
+        }
+      }
+    return false;
+  }
+  
+  function colChecked(matrix, col, num){
+    for(var i = 0; i < 9; i++){
+      if(matrix[i][col] == num){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+return false;
 }
